161066464ad41f0c158be325c79b285e9df13d7e
arullll <108324657+idzharulhaq@users.noreply.github.com>
2022-07-22 20:10:53 +0700

    Add files via upload

---
+++ myname.js
@@ -0,0 +1,54 @@
+// name:ARUL
+document.write("<br>")
+for (let y = 1; y<10; y++){
+      for (let x = 1; x<10; x++){
+              if(x==1 && y>=5 || x+y==6 || x-y==4 || y==5 || y>=5 && x==9){
+                      document.writeln("x")
+                  }
+                  else {
+          document.writeln('&ensp;')
+      }
+  }
+  document.writeln("<br>")
+} 
+document.writeln('<br>')
+for (let y = 1; y<10; y++){
+      for (let x = 1; x<10; x++){
+              if(x==1 || x<8 && y==1 || x<8 && y==5 || x==y && x>5 && y>5){
+                  document.writeln("x")
+              }
+              else if (x==8 && y==2 || x==9 && y==3 || x==8 && y==4){
+                  document.write('x')
+              }
+              else {
+                  document.writeln('&ensp;')
+      }
+  }
+  document.writeln("<br>")
+}
+document.write("<br>")
+  for(let y = 1; y<10; y++){
+      for (let x = 1; x<10; x++){
+              if(x==9 || y==9|| x==1 ){
+                      document.writeln("x")
+                  }
+                  else {
+          document.writeln('&ensp;')
+      }
+  }
+  document.writeln("<br>")
+  }
+document.write("<br>")
+
+  for (let y = 1; y<10; y++){
+      for (let x = 1; x<10; x++){
+              if(y==9|| x==1 ){
+                      document.writeln("x")
+                  }
+                  else {
+          document.writeln('&ensp;')
+      }
+  }
+  document.writeln("<br>")
+  }
+  document.writeln('<br>')
